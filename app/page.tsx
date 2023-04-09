b22397932f2ce0e9f19ab67e9fb46297f6c16864
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-04-09 04:16:55 -0300

    Fixed height bug on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "./AuthForm";
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen items-center justify-evenly">
+    <main className="flex absolute inset-0 items-center justify-evenly">
       <Image
         src="/fiufit.svg"
         className="hidden lg:block invert dark:invert-0"
