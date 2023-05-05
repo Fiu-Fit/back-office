c240dfbec552a5d7d6debc3d8acbbfde75774ebe
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-05 03:01:29 -0300

    Fixed pagination control disappearing

--- app/(dashboard)/workouts/[id]/components/ExerciseList.tsx
+++ app/(dashboard)/workouts/[id]/components/ExerciseList.tsx
@@ -11,8 +11,8 @@ export default function ExerciseList({
   className?: string;
 }) {
   return (
-    <div className={`h-full rounded-md overflow-hidden ${className}`}>
-      <div className='h-full dark:bg-gray-950 overflow-y-scroll'>
+    <div className={`h-full rounded-md overflow-hidden flex flex-col ${className}`}>
+      <div className='h-full flex-shrink dark:bg-gray-950 overflow-y-scroll'>
         <table className='w-full divide-y divide-gray-200 dark:divide-black'>
           <thead>
             <tr className='sticky top-0 bg-gray-50 dark:bg-zinc-900 z-50'>
@@ -34,14 +34,17 @@ export default function ExerciseList({
                     header != '' && <td className='px-6 py-4'> {header} </td>
                 )}
                 <td className='px-6 py-4'>
-                  <Button href='#' LinkComponent={Link}> Detalles </Button>
+                  <Button href='#' LinkComponent={Link}>
+                    {' '}
+                    Detalles{' '}
+                  </Button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <div className='bg-gray-50 dark:bg-zinc-900 px-4 py-3'>
+      <div className='block bg-gray-50 dark:bg-zinc-900 px-4 py-3'>
         <div className='flex justify-between'>
           <button className='px-3 py-2 font-medium rounded-md dark:bg-zinc-950 text-gray-700 dark:text-gray-400 hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-black active:text-gray-800 transition ease-in-out duration-150'>
             Anterior
