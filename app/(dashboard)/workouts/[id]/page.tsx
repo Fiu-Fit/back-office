e01ad67b8695d0437ae99f635b4cd8be39576664
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-05 03:01:40 -0300

    Format

--- app/(dashboard)/workouts/[id]/page.tsx
+++ app/(dashboard)/workouts/[id]/page.tsx
@@ -10,17 +10,17 @@ async function getWorkout(id: number): Promise<Workout> {
   const workout: Workout = await new Promise(resolve => {
     setTimeout(() => {
       resolve({
-        id: id,
+        id:   id,
         name: 'Nombre',
         description:
           '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-        authorId: 1,
-        duration: 1,
-        category: Category.CARDIO,
+        authorId:   1,
+        duration:   1,
+        category:   Category.CARDIO,
         difficulty: 1,
-        updatedAt: new Date().toDateString(),
+        updatedAt:  new Date().toDateString(),
         athleteIds: [1, 2, 3],
-        exercises: [],
+        exercises:  [],
       });
     }, 1000);
   });
