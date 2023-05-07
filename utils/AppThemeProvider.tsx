715c1b87d0eb885ac10413514cae9643488ffda8
Manuel Sanchez <msanchezf@fi.uba.ar>
2023-05-07 02:08:06 -0300

    Dark mode based on system preference for mui (#12)

--- utils/AppThemeProvider.tsx
+++ utils/AppThemeProvider.tsx
@@ -1,15 +1,17 @@
 'use client';
 
+import { useMediaQuery } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import React from 'react';
 
-const innerTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 export default function AppThemeProvider(props: { children: React.ReactNode }) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const innerTheme = createTheme({
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light',
+    },
+  });
+
   const { children } = props;
   return <ThemeProvider theme={innerTheme}>{children}</ThemeProvider>;
 }
