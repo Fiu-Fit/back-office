9c50286354a8caa4457d53a7c118abba258376af
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-09 05:03:29 -0300

    API connection

--- app/(dashboard)/workouts/page.tsx
+++ app/(dashboard)/workouts/page.tsx
@@ -1,42 +1,11 @@
 import { mdiDumbbell } from '@mdi/js';
-import { Category } from './interfaces/Category';
+import api from '@/api/serverSideAxiosConfig';
 import { WorkoutList } from '@/app/(dashboard)/workouts/components/WorkoutList';
 import { Workout } from '@/app/(dashboard)/workouts/interfaces/Workout';
 import { ControlHeader } from '@/components';
 
 async function getWorkouts(): Promise<Workout[]> {
-  // const { data: workoutList } = await api.get<Workout[]>('/workouts');
-
-  const workoutList: Workout[] = await new Promise(resolve => {
-    setTimeout(() => {
-      resolve([
-        {
-          id:          123,
-          name:        'Crossfit',
-          description: 'Ideal para perder peso',
-          authorId:    1,
-          duration:    1,
-          category:    Category.CARDIO,
-          difficulty:  1,
-          updatedAt:   new Date().toDateString(),
-          athleteIds:  [1, 2, 3],
-          exercises:   [],
-        },
-        {
-          id:          124,
-          name:        'Brazos',
-          description: 'Solo apto para avanzados',
-          authorId:    2,
-          duration:    20,
-          category:    Category.ARMS,
-          difficulty:  3,
-          updatedAt:   new Date().toDateString(),
-          athleteIds:  [1, 2, 3],
-          exercises:   [],
-        },
-      ]);
-    }, 1000);
-  });
+  const { data: workoutList } = await api.get<Workout[]>('/workouts');
 
   return workoutList;
 }
