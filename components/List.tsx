87256d7fbd8ed2efd11214dc6ea0d76e49274180
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-09 22:02:01 -0300

    Nav blur

--- components/List.tsx
+++ components/List.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { Button } from '@mui/material';
 import Link from 'next/link';
 
 export default function List({
@@ -19,7 +16,7 @@ export default function List({
       <div className='h-full flex-shrink dark:bg-gray-950 overflow-y-scroll'>
         <table className='w-full divide-y divide-gray-200 dark:divide-black'>
           <thead>
-            <tr className='sticky top-0 bg-gray-50 dark:bg-zinc-900 z-50'>
+            <tr className='sticky top-0 bg-gray-50 dark:bg-zinc-900 z-40'>
               {Object.keys(headers).map(header => (
                 <th
                   key={header}
@@ -42,9 +39,9 @@ export default function List({
 
                 {detailButtonHref && (
                   <td className='px-6 py-4'>
-                    <Button href={`${detailButtonHref}/${value[headers['ID']]}`} LinkComponent={Link}>
+                    <Link href={`${detailButtonHref}/${value[headers['ID']]}`} className='btn'>
                       Detalles
-                    </Button>
+                    </Link>
                   </td>
                 )}
               </tr>
@@ -54,10 +51,10 @@ export default function List({
       </div>
       <div className='block bg-gray-50 dark:bg-zinc-900 px-4 py-3'>
         <div className='flex justify-between'>
-          <button className='px-3 py-2 font-medium rounded-md dark:bg-zinc-950 text-gray-700 dark:text-gray-400 hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-black active:text-gray-800 transition ease-in-out duration-150'>
+          <button className='btn btn-primary'>
             Anterior
           </button>
-          <button className='px-3 py-2 font-medium rounded-md dark:bg-zinc-950 text-gray-700 dark:text-gray-400 hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-black active:text-gray-800 transition ease-in-out duration-150'>
+          <button className='btn btn-primary'>
             Siguiente
           </button>
         </div>
