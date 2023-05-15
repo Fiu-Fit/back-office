5b39c6214052c65590d3c86faf92507168172825
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-15 17:45:46 -0300

    Added status modals

--- app/(dashboard)/register/RegisterForm.tsx
+++ app/(dashboard)/register/RegisterForm.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { User } from '@fiu-fit/common';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { useRef, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
-import { Button, Form, TextInput } from '@/components';
+import { Button, ErrorModal, Form, Modal, TextInput } from '@/components';
 import { validateEmail, validateName, validatePassword } from '@/utils';
 
 const ADMIN_ROLE = 'Admin';
 
 export default function RegisterForm() {
+  const successModalRef = useRef<HTMLInputElement | null>(null);
+  const errorModalRef = useRef<HTMLInputElement | null>(null);
+  const [error, setError] = useState<string>('null');
+
   const {
     register,
     handleSubmit,
@@ -55,7 +60,7 @@ export default function RegisterForm() {
 
   const onSubmit = async (formData: FieldValues) => {
     const registerData: User = {
-      ...formData as User,
+      ...(formData as User),
       role:       ADMIN_ROLE,
       bodyWeight: 1,
     };
@@ -64,27 +69,48 @@ export default function RegisterForm() {
       await axios.post('/api/auth/register', registerData);
     } catch (err) {
       console.error(err);
+      if (!(err instanceof AxiosError)) {
+        setError('Error desconocido');
+        return;
+      }
+
+      setError(`${err.response?.status} - ${err.response?.statusText}`);
     }
   };
 
   return (
-    <Form
-      onSubmit={handleSubmit(data => onSubmit(data))}
-      className='grid grid-cols-2 gap-4 w-11/12 max-w-lg my-auto'
-    >
-      {inputs.map(input => (
-        <TextInput
-          name={input.name}
-          key={input.name}
-          label={input.label}
-          type={input.type}
-          register={register}
-          validation={input.validation}
-          errorMessage={errors[input.name]?.message as string}
-          containerClassName={input.halfWidth ? 'col-span-1' : 'col-span-2'}
-        />
-      ))}
-      <Button text='Registrar' type='submit' className='col-span-2' />
-    </Form>
+    <>
+      <Form
+        onSubmit={handleSubmit(data => onSubmit(data))}
+        className='grid grid-cols-2 gap-4 w-11/12 max-w-lg my-auto'
+      >
+        {inputs.map(input => (
+          <TextInput
+            name={input.name}
+            key={input.name}
+            label={input.label}
+            type={input.type}
+            register={register}
+            validation={input.validation}
+            errorMessage={errors[input.name]?.message as string}
+            containerClassName={input.halfWidth ? 'col-span-1' : 'col-span-2'}
+          />
+        ))}
+        <Button text='Registrar' type='submit' className='col-span-2' />
+      </Form>
+      <Modal id='register-modal' innerRef={successModalRef}>
+        <h1 className='text-lg font-bold'>¡Registro exitoso!</h1>
+        <p className='mb-4'>
+          Se ha enviado un correo electrónico al usuario con las instrucciones
+          para completar el registro.
+        </p>
+        <Button text='Aceptar' onClick={() => successModalRef.current?.click()} />
+      </Modal>
+      <ErrorModal
+        title='Oops... Ocurrio un problema'
+        error={error}
+        innerRef={errorModalRef}
+      />
+    </>
   );
 }
