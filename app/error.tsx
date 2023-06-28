5c2fcbff7994310686f333bd4d4e1fa961f8434d
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-06-28 14:06:13 -0300

    Update error message overflow

--- app/error.tsx
+++ app/error.tsx
@@ -12,9 +12,9 @@ export default function Error({
   return (
     <Modal id='error-modal' type='alwaysShown'>
       <h1 className='text-lg font-bold'>¡Algo salio mal!</h1>
-      <div className='mb-4'>
-        <pre>{error.message}</pre>
-        {!!error.cause && <pre>{!error.cause}</pre>}
+      <div className='mb-4 max-h-96 overflow-y-auto'>
+        <code>{error.message}</code>
+        {!!error.cause && <code>{!error.cause}</code>}
       </div>
       <Button text='Reintentar' onClick={() => reset()} />
     </Modal>
